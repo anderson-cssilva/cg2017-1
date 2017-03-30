dd51abe14205c0e6794c5d727ef33c3bf32c8e5b
Anderson Silva <anderson.cssilva@gmail.com>
2017-03-30 13:02:07 -0300

    fundo e haste

--- trabalho1/trab1.c
+++ trabalho1/trab1.c
@@ -1,143 +1,96 @@
 #include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 
 #include <GL/freeglut.h>
 
-GLfloat R = 0.0f, G = 0.0f, B = 0.0f;
+/* 
+ * Posição do topo da haste do cata vento.
+ * É o centro do conjunto de triângulos que formam a hélice.
+ */
+GLfloat xpos = 300.0f, ypos = 250.0f;
 
-GLuint background_texture;
+/* Angulo para movimento da hélice */
+GLfloat theta = 0.0f;
 
-GLuint loadBMP(const char * imagepath) 
+void desenhaFundo()
 {
-	// Data read from the header of the BMP file
-	
-	// Each BMP file begins by a 54-bytes header
-	unsigned char header[54];
-	
-	// Position in the file where the actual data begins
-	unsigned int dataPos;
-	unsigned int width, height;
-	
-	// = width*height*3
-	unsigned int imageSize;
+	/* Desenha grama verde */
+	glColor3f(0.0f, 1.0f, 0.0f);
+	glBegin(GL_QUADS);
+		glVertex2f(0.0f, 0.0f);
+		glVertex2f(0.0f, 150.0f);
+		glVertex2f(400.0f, 150.0f);
+		glVertex2f(400.0f, 0.0f);
+	glEnd();
 
-	// Actual RGB data
-	unsigned char * data;
-
-	// Open the file
-	FILE * file = fopen(imagepath,"rb");
-	if (!file) {
-		printf("%s: Background image could not be opened\n", imagepath); 
-		exit(1);
-	}
-	
-	// If not 54 bytes read -> problem
-	if ( fread(header, 1, 54, file) != 54 ) {
-		printf("%s: Not a correct BMP file\n", imagepath);
-		exit(1);
-	}
-
-	if ( header[0]!='B' || header[1]!='M' ){
-		printf("%s: Not a correct BMP file\n", imagepath);
-		exit(1);
-	}
-
-	// Read ints from the byte array
-	dataPos    = *(int*)&(header[0x0A]);
-	imageSize  = *(int*)&(header[0x22]);
-	width      = *(int*)&(header[0x12]);
-	height     = *(int*)&(header[0x16]);
-	
-	// Some BMP files are misformatted, guess missing information
-	if (imageSize == 0) {
-		// 3 : one byte for each Red, Green and Blue component
-		imageSize = width*height*3; 
-	}
-	if (dataPos == 0) {
-		// The BMP header is done that way
-		dataPos = 54; 
-	}
-
-	// Create a buffer
-	data = new unsigned char [imageSize];
-	
-	// Read the actual data from the file into the buffer
-	fread(data, 1, imageSize, file);
-	
-	// Everything is in memory now, the file can be closed
-	fclose(file);
-
-	// Create one OpenGL texture
-	GLuint textureID;
-	glGenTextures(1, &textureID);
-	
-	// "Bind" the newly created texture : all future texture functions will modify this texture
-	glBindTexture(GL_TEXTURE_2D, textureID);
-
-	// Give the image to OpenGL
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexImage2D(GL_TEXTURE_2D, 0,GL_RGBA, width, height, 0, GL_BGR, GL_UNSIGNED_BYTE, data);
-	
-	//glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
-	//glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR_MIPMAP_NEAREST);
-	//gluBuild2DMipmaps(GL_TEXTURE_2D, 3, width, height, GL_RGB, GL_UNSIGNED_BYTE, data);
-
-	delete data;
-	return textureID;
+	/* Desenha céu azul*/
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glBegin(GL_QUADS);
+		glVertex2f(0.0f, 150.0f);
+		glVertex2f(0.0f, 400.0f);
+		glVertex2f(400.0f, 400.0f);
+		glVertex2f(400.0f, 150.0f);
+	glEnd();
 }
 
-GLfloat sorteia_cor() 
+void desenhaHaste()
 {
-	return (rand()*1.0f) / (RAND_MAX*1.0f);
+	/* Desenha haste preta */
+	glColor3f(.0f, .0f, .0f);
+	glBegin(GL_QUADS);
+		glVertex2f(299.0f, 100.0f); // botton-left
+		glVertex2f(299.0f, 250.0f); // upper -left
+		glVertex2f(301.0f, 250.0f); // upper-right
+		glVertex2f(301.0f, 100.0f); // botton-right
+	glEnd();
+}
+
+void aplicaTransformacoes()
+{
+	/*
+		glTranslatef(xpos, ypos, 0.0f);
+		glRotatef(theta, 0.0f, 0.0f, 1.0f);
+		glTranslatef(-xpos, -ypos, 0.0f);
+	 */
+}
+
+void desenhaHelice()
+{
+
 }
 
 void desenha(void) 
 {
 	/* Colorir o fundo da cena de branco */
-	//glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
-	//glClear(GL_COLOR_BUFFER_BIT);
+	glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
 
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);	// Clear Screen And Depth Buffer
-	/*
-	glColor3f(R, G, B);
-	glBegin(GL_QUADS);
-		glVertex2f(100.0f, -100.0f);
-		glVertex2f(100.0f, 100.0f);
-		glVertex2f(300.0f, 100.0f);
-		glVertex2f(300.0f, -100.0f);
-	glEnd();
-	*/
+	/* Desenha o fundo da tela */
+	desenhaFundo();
 
-	glEnable(GL_TEXTURE_2D);
-	glBindTexture(GL_TEXTURE_2D, background_texture);
-	glColor4f(1.0, 1.0, 1.0, 1.0); // reset gl color
-	glBegin( GL_QUADS );
-	glTexCoord2d(0.0,0.0); glVertex3d(100.0f, -100.0f, -1.0);
-	glTexCoord2d(0.0,1.0); glVertex3d(100.0f, 100.0f, -1.0);
-	glTexCoord2d(1.0,1.0); glVertex3d(300.0f, 100.0f, -1.0);
-	glTexCoord2d(1.0,0.0); glVertex3d(300.0f, -100.0f, -1.0);
-	glEnd();
-	glDisable(GL_TEXTURE_2D);
+	/* Desenha a primeira parte do cata-vento, a Haste, que é fixa */
+	desenhaHaste();
+
+	/* Aplica as tranformações necessárias para o movimento das hélices */
+	aplicaTransformacoes();
+
+	/* Desenha os triângulos que representam as hélices */
+	desenhaHelice();
 
 	glFlush();
 }
 
-//		Eventos 
-void on_mouseClick(int botao_clicado, int estado_do_click, int x_mouse_position, int y_mouse_position) {
+/* Evento de click no mouse */
+void on_mouseClick(int botao_clicado, int estado_do_click, 
+		int x_mouse_position, int y_mouse_position) {
 	
 	if(estado_do_click == GLUT_UP) {
 		if(botao_clicado == GLUT_LEFT_BUTTON) {
-			R = 0.0f;
-			G = 0.0f;
-			B = 0.0f;
+			theta += 10.0f;
 		}
 
 		if(botao_clicado == GLUT_RIGHT_BUTTON) {
-			R = sorteia_cor();
-			G = sorteia_cor();
-			B = sorteia_cor();
+			theta -= 10.0f;
 		}
 	}
 
@@ -147,20 +100,27 @@ void on_mouseClick(int botao_clicado, int estado_do_click, int x_mouse_position,
 
 int main(int argc, char* argv[])
 {
-	srand(time(NULL));
+	/* Inicia uma instância da glut */
+	glutInit(&argc, argv); 
 
-	glutInit(&argc, argv); // Inicia uma instância da glut
-	//glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); // Define o modo de display do buffer
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-	glutInitWindowSize(800, 600); // Define o tamanho em pixels da janela
-	glutCreateWindow("Exemplo OpenGL"); // Define o título da janela
-	glutDisplayFunc(desenha); // Estabelece que a função de rendering é a função "desenha(.)"
+	/* Define o modo de display do buffer */
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); 
 
-	glutMouseFunc(on_mouseClick); // Evento de click do mouse
+	/* Define o tamanho em pixels da janela */
+	glutInitWindowSize(800, 600);
 
-	gluOrtho2D(0, 400, -150, 150); // Define o plano ortogonal em que a cena será construída
+	/* Define o título da janela */
+	glutCreateWindow("Exemplo OpenGL");
 
-	background_texture = loadBMP("./data/xp.bmp");
+	/* Estabelece que a função de rendering é a função "desenha(.)" */
+	glutDisplayFunc(desenha);
 
-	glutMainLoop(); // Inicia as operações conforme as especificações anteriores
+	/* Tratamento de eventios de click do mouse */
+	glutMouseFunc(on_mouseClick);
+
+	/* Define o plano ortogonal em que a cena será construída */
+	gluOrtho2D(0, 400, 0, 400); 
+
+	/* Inicia as operações conforme as especificações anteriores */
+	glutMainLoop();
 }
