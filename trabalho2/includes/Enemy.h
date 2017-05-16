6335a7b32a97c88b5c5e3e30e004e2fd5486bdb2
robsonpessoa <pessoa.robsonm@gmail.com>
2017-05-16 18:24:46 -0300

    Refactored names and added some comments in the class headers.

--- trabalho2/includes/Enemy.h
+++ trabalho2/includes/Enemy.h
@@ -10,19 +10,28 @@
 #include "Shooter.h"
 #include <vector>
 
+/* Esta classe abstrata provê a representação de um inimigo, o qual
+ * o usuário utilizará como personagem chave no jogo.
+ *
+ * Ela é uma subclasse de Shooter, pois performa tiros para
+ * derrotar o usuário.
+ */
 class Enemy : Shooter {
 public:
     Enemy(GLfloat x, GLfloat y);
-    virtual void draw() = 0;
+	// métodos responsáveis pelo desenho e movimentação do jato
+	virtual void draw() = 0;
     void move(int step);
-    Shoot* shoot();
-    void shoot_done();
 
+	// métodos responsáveis pelo controle do tiro
+    Shoot* shoot();
+    void hit_target();
+	bool has_shot();
+	bool is_active();
+
+	// métodos get
     GLfloat get_x();
     GLfloat get_y();
-
-	bool is_active();
-	bool has_shot();
 
 protected:
     GLfloat x_pos, y_pos;
